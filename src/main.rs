27493a71697e2a1ce52353f2b9444f32b395220a
erik-bobinski <ebobinski0@gmail.com>
2026-07-08 10:03:00 -0400

    Revert "web server serves static site for riot demo - need production api key for valorant APIs"
    This reverts commit b0783626d1fe3f491699e5fdae8d3b864cf2ad1f.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ mod discord;
 mod polling;
 mod riot_api;
 mod types;
-mod web;
 
 #[tokio::main]
 async fn main() {
@@ -19,12 +18,6 @@ async fn main() {
         dotenv::dotenv().ok();
         env::var("DISCORD_TOKEN").expect("Expected a discord bot env var in environment")
     });
-
-    let port: u16 = env::var("PORT")
-        .ok()
-        .and_then(|p| p.parse().ok())
-        .unwrap_or(8080);
-    tokio::spawn(web::serve(port));
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
